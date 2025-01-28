adb26b4cd741f04105a454949e9854e2a00277f2
malcolmling97 <malcolmling97@gmail.com>
2025-01-29 00:11:17 +0800

    added htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,23 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        
+        final_output = ""
+        for _ in self.props:
+            final_output+=(f' {_}="{self.props[_]}"')
+
+        return(final_output)
+         
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+        
